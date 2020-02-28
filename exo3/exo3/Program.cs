288f59fd295ed496e6ae45bc3eabb113908b3dba
Coulibaly Kafalo Alex <ckalexis20@gmail.com>
2020-02-28 21:12:27 +0000

    Ajout de l'exo 3

---
+++ exo3/exo3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Pour l'énoncé aller sur le lien sur suivant : https://github.com/Soro08/NAN320 */
+using System;
+
+namespace exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez le réel a :");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            int n = 1;
+            double s = 0;
+
+            while(s <= a)//Détermination de l'entier n
+            {
+                s += (double)1 / n;
+                n += 1;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
