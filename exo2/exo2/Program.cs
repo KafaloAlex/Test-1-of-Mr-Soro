ab3c176101f2b2e5659495238003a0797b7708c7
Coulibaly Kafalo Alex <ckalexis20@gmail.com>
2020-02-28 20:48:07 +0000

    Ajout de l'exercice 2

---
+++ exo2/exo2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Écrire le même algorithme, mais en utilisant deux variables booléennes pour vérifier l’état de l’eau, 
+ * sans comparaisons dans les instructions if*/
+using System;
+
+namespace exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez la Température de l'eau : ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+
+            bool etatLiquide = (temp >= 1 && temp <= 100);
+            bool etatGlace = (temp <= 0);
+
+            if(etatLiquide)
+                Console.WriteLine("L'eau est à {0} °C, elle est donc à l'état 'Liquide'",temp);
+            else if(etatGlace)
+                Console.WriteLine("L'eau est à {0} °C, elle est donc à l'état de 'Glace'", temp);
+            else
+                Console.WriteLine("L'eau est à {0} °C, elle est donc à l'état de 'Vapeur'", temp);
+        }
+    }
+}
